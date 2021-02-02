ce8e7375f2cf6915e40c70ebedf4dcf5d6a6a2e5
Tauane Costa <tauanedasilvacosta@gmail.com>
2021-02-02 19:41:51 -0300

    Adding EsLint, Prettier and EditorConfig

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,13 @@
 import express from 'express';
+import routes from './routes';
 
 const app = express();
 
-app.get('/', (request, response) => {
-    return response.json({ message: 'Hello World'});
-});
+app.use(express.json());
+app.use(routes);
+
+app.get('/', (request, response) => response.json({ message: 'Hello World' }));
 
 app.listen(3333, () => {
-    console.log('server started')
+  console.log('server started');
 });
