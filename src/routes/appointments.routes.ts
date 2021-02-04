e29db53368f55eff7fbd45b645cade000a32a2f8
Tauane Costa <tauanedasilvacosta@gmail.com>
2021-02-03 22:33:38 -0300

    Added Service Class

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -1,7 +1,8 @@
 import { Router } from 'express';
-import { startOfHour, parseISO, isEqual } from 'date-fns';
+import { parseISO } from 'date-fns';
 import AppointmentsRepository from '../repositories/AppointmentsRepository';
 import Appointment from '../models/Appointment';
+import CreateAppointmentService from '../services/CreateAppointmentService';
 
 const appoinmentsRouter = Router();
 const appointmentsRepository = new AppointmentsRepository();
@@ -13,22 +14,22 @@ appoinmentsRouter.get('/', (request, response) => {
 });
 
 appoinmentsRouter.post('/', (request, response) => {
-    const { provider, date } = request.body;
+    try {
+        const { provider, date } = request.body;
 
-    const parsedDate = startOfHour(parseISO(date));
+        const parsedDate = parseISO(date);
 
-    const findAppointmentInSameDate = appointmentsRepository.findByDate(parsedDate);
+        const createAppointment = new CreateAppointmentService(appointmentsRepository);
 
-    if(findAppointmentInSameDate){
-        return response.status(400).json({ message: 'This appointment is already booked' });
+        const appointment = createAppointment.execute({
+            date: parsedDate,
+            provider })
+
+        return response.json(appointment);
     }
-
-    const appointment = appointmentsRepository.create({
-        provider,
-        date: parsedDate
-    });
-
-    return response.json(appointment) ;
+    catch (err) {
+        return response.status(400).json({ error: err.message });
+    }
 })
 
 export default appoinmentsRouter;
